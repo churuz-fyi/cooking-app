7f29878f4c1141991712a780e2c35b884b8444d6
Jesus Ruiz Ramos <94080165+churuizramos@users.noreply.github.com>
2024-11-25 15:30:02 -0500

    added more buttons that were missing on the navar (#4)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,10 @@
 <template>
     <nav class="flex flex-row gap-4 bg-amber-300 p-3">
         <button>Home</button>
+        <button><img src="../assets/icons8-add-50.png" alt="create new recipe"/></button>
         <input class="p-2 w-full rounded-full" type="text" id="searchBox" placeholder="Search recipes"/>
-        <button><img src="/icons8-user-40.png" alt="User temporary icon."/></button>
+        <button>Recipes</button>
+        <button>Cookbooks</button>
+        <button><img src="/icons8-user-40.png" alt="user page"/></button>
     </nav>
 </template>
