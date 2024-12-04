a5764a35f0ee8f1c7e716359dc73a30ec0ec646a
churuizramos <ruizramos04@outlook.com>
2024-12-04 12:13:55 -0500

    added Recipe request for API and formatting for all the data

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,17 +1,36 @@
+<script setup>
+import axios from 'axios';
+import { guardReactiveProps, onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const recipe = ref([]);
+
+onMounted( async () => {
+    try {
+        const response = await axios.get(`http://localhost:5000/get_recipe/${route.params.id}`);
+        recipe.value = response.data;
+        console.log("Recipe:",recipe.value);
+    } catch (error) {
+        console.error("Error fetching recipe:", error);
+    }
+});
+</script>
 <template>
-    <div class = "border-2 rounded-md border-black bg-amber-100 divide-y divide-solid divide-black">
-        <p class="text-center text-xl font-bold">Recipe</p>
+    <div v-for="(item, index) in recipe" :key="index" class = "border-2 rounded-md border-black bg-amber-100 divide-y divide-solid divide-black mx-40">
+        <p class="text-center text-xl font-bold">{{ item.title }} # {{ item.id }}</p>
+        <p class="text-center text-lg">{{ item.userId }}</p>
         <div class = "flex flex-col p-4 divide-y divide-solid divide-black">
-            <p class="pb-1">Description: </p>
-            <p class="pb-1">Meal Type: </p>
-            <p class="pb-1">Time of Meal: </p>
+            <p class="pb-1">Description: {{ item.description }}</p>
+            <p class="pb-1">Meal Type: {{ item.mealType }}</p>
+            <p class="pb-1">Time of Meal: {{ item.timetoeat }}</p>
             <div class="flex flex-row pb-1">
-                <p>Prep time: </p>
-                <p class="px-4">Cook time: </p>
+                <p>Prep time: {{ item.prepTime }}</p>
+                <p class="px-4">Cook time: {{ item.cookTime }}</p>
             </div>
-            <p class="pb-1">Yield: </p>
-            <p class="pb-1">Country: </p>
-            <p class="pb-1">Season: </p>
+            <p class="pb-1">Yield: {{ item.recyield }}</p>
+            <p class="pb-1">Country: {{ item.countryOfOrigin }}</p>
+            <p class="pb-1">Season: {{ item.season }}</p>
         </div>
     </div>
 </template>
