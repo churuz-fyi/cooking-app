4f7bf9549209fc86aa7efff161ffa6be1b4a60f8
GreyNeedham <jgneed01@louisville.edu>
2024-12-03 21:10:13 -0500

    Updated Recipe View

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,15 +1,17 @@
 <template>
     <div class = "border border-2 rounded-md border-black bg-amber-100 divide-y divide-solid divide-black">
-        <p class="text-center text-lg font-bold">Recipe</p>
+        <p class="text-center text-xl font-bold">Recipe</p>
         <div class = "flex flex-col p-4 divide-y divide-solid divide-black">
             <p class="pb-1">Description: </p>
             <p class="pb-1">Meal Type: </p>
+            <p class="pb-1">Time of Meal: </p>
             <div class="flex flex-row pb-1">
                 <p>Prep time: </p>
                 <p class="px-4">Cook time: </p>
             </div>
             <p class="pb-1">Yield: </p>
             <p class="pb-1">Country: </p>
+            <p class="pb-1">Season: </p>
         </div>
     </div>
 </template>
