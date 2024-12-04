ae3dfa6e23bf36948130b8c4a47a50f65ff24475
churuizramos <ruizramos04@outlook.com>
2024-12-03 21:12:57 -0500

    blank placeholder user view

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <div class="text-2xl p-4 bg-white">
+        <h1>User Dashboard</h1>
+        <div class="flex flex-row gap-3">
+            <p>USERNAME</p>
+            <p># USERID</p>
+        </div>
+        <div class="flex flex-row gap-3">
+            <p>FIRSTNAME LASTNAME</p>
+            <p>JOINDATE</p>
+        </div>
+        <button @click="auth.logout">Logout</button>
+    </div>
+</template>
