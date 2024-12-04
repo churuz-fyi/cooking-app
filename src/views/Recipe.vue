ee328f7dba78a4d5d66026c0ea9e40f46b0fee18
GreyNeedham <jgneed01@louisville.edu>
2024-12-03 21:02:30 -0500

    Recipe-view

---
+++ src/views/Recipe.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "border border-2 rounded-md border-black bg-amber-100 divide-y divide-solid divide-black">
+        <p class="text-center text-lg font-bold">Recipe</p>
+        <div class = "flex flex-col p-4 divide-y divide-solid divide-black">
+            <p class="pb-1">Description: </p>
+            <p class="pb-1">Meal Type: </p>
+            <div class="flex flex-row pb-1">
+                <p>Prep time: </p>
+                <p class="px-4">Cook time: </p>
+            </div>
+            <p class="pb-1">Yield: </p>
+            <p class="pb-1">Country: </p>
+        </div>
+    </div>
+</template>
