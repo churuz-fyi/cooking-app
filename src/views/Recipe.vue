c820f342805022447556bb3442f3d713b49b1bf2
churuizramos <ruizramos04@outlook.com>
2024-12-04 12:57:16 -0500

    implemented the ability to inspect user data by clicking their name on a recipe

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,25 +1,32 @@
 <script setup>
 import axios from 'axios';
-import { guardReactiveProps, onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 const recipe = ref([]);
 
 onMounted( async () => {
     try {
         const response = await axios.get(`http://localhost:5000/get_recipe/${route.params.id}`);
         recipe.value = response.data;
-        console.log("Recipe:",recipe.value);
+        console.log("User Data:",recipe.value);
     } catch (error) {
         console.error("Error fetching recipe:", error);
     }
 });
+
+const goToUser = (user_id) => {
+    console.log('goToUser function triggered');
+    console.log(user_id);
+    router.push({ name: 'User Page', params: {user_id: user_id}});
+};
 </script>
 <template>
     <div v-for="(item, index) in recipe" :key="index" class = "border-2 rounded-md border-black bg-amber-100 divide-y divide-solid divide-black mx-40">
         <p class="text-center text-xl font-bold">{{ item.title }} # {{ item.id }}</p>
-        <p class="text-center text-lg">{{ item.userId }}</p>
+        <p @click="goToUser(item.userId_id)" class="text-center text-lg">{{ item.userId }}</p>
         <div class = "flex flex-col p-4 divide-y divide-solid divide-black">
             <p class="pb-1">Description: {{ item.description }}</p>
             <p class="pb-1">Meal Type: {{ item.mealType }}</p>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,17 +1,31 @@
 <script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+const user_data = ref([]);
+
+onMounted( async () => {
+    try {
+        const response = await axios.get(`http://localhost:5000/get_user/${route.params.user_id}`);
+        user_data.value = response.data;
+        console.log("Recipe:",user_data.value);
+    } catch (error) {
+        console.error("Error fetching recipe:", error);
+    }
+});
 </script>
 <template>
-    <div class="text-2xl p-4 bg-white">
+    <div v-for="(item, index) in user_data" :key="index" class="text-2xl p-4 bg-white">
         <h1>User Dashboard</h1>
-        <div class="flex flex-row gap-3">
-            <p>USERNAME</p>
-            <p># USERID</p>
+        <div class="flex flex-row gap-3 font-bold">
+            <p>{{item.username}}</p>
+            <p># {{ item.id }}</p>
         </div>
         <div class="flex flex-row gap-3">
-            <p>FIRSTNAME LASTNAME</p>
-            <p>JOINDATE</p>
+            <p>{{item.firstName}} {{item.lastName}}</p>
+            <p>Joined: {{ item.joinDate }}</p>
         </div>
-        <button @click="auth.logout">Logout</button>
     </div>
 </template>
